7231ac07e0692c8075a22674bc2ff7bd25797f91
Kieran Campbell <kieranrcampbell@gmail.com>
2016-05-31 15:13:43 +0100

    Now generate realistic branching data

--- scripts/synthetic.R
+++ scripts/synthetic.R
@@ -1,5 +1,7 @@
 library(ggplot2)
+library(rhdf5)
 
+set.seed(123L)
 
 #' Sigmoid function for activations
 sigmoid <- function(t, phi, k, delta) {
@@ -27,10 +29,22 @@ inds <- 1:(G / 2)
 inds2 <- (G/2 + 1):G
 k[, 2] <- k[inds, 1]
 
-k[inds2, ] <- apply(k[inds2, ], 1, function(r) r * sample(c(0, 1)))
+k[inds2, ] <- t(apply(k[inds2, ], 1, function(r) r * sample(c(0, 1))))
 
-phi[, 2] <- phi[inds, 2]
-delta[, 2] <- delta[inds, 1] <- runif(G / 2, 0, 0.5)
+phi[, 1] <- phi[, 2]
+delta[inds, 2] <- delta[inds, 1] <- runif(G / 2, 0, 0.5)
+
+## Now make it look like a branching process
+for(r in inds2) {
+  whichzero <- which(k[r,] == 0)
+  nonzero <- which(k[r,] != 0)
+  k_sign <- sign(k[r,nonzero])
+  if(k_sign == 1) {
+    phi[r, whichzero] <- 0
+  } else {
+    phi[r, whichzero] <- 2 * phi[r, nonzero]
+  }
+}
 
 pst <- runif(C)
 
@@ -42,6 +56,20 @@ X <- sapply(seq_along(branch), function(i) {
   rnorm(length(mu), mu, gsd)
 })
 
+fname <- file.path("data", "synthetic.h5")
+if(!file.exists(fname)) {
+  h5createFile(fname)
+}
+
+h5createGroup(fname, "basic_branching") # we'll call this dataset basic_branching
+h5write(t(X), fname, "basic_branching/X")
+h5write(pst, fname, "basic_branching/pseudotime")
+h5write(branch, fname, "basic_branching/branch_assignment")
+h5write(k, fname, "basic_branching/k")
+h5write(phi, fname, "basic_branching/phi")
+h5write(delta, fname, "basic_branching/delta")
+h5write(gsd, fname, "basic_branching/stdev")
+
 d <- data.frame(t(X), pseudotime = pst, branch = as.factor(branch))
 
 dm <- reshape2::melt(d, id.vars = c("pseudotime", "branch"),
@@ -50,3 +78,4 @@ dm <- reshape2::melt(d, id.vars = c("pseudotime", "branch"),
 ggplot(dm, aes(x = pseudotime, y = expression, colour = branch)) +
   geom_point() + facet_wrap(~ gene, scales = "free_y")
 
+ggsave("figs/basic_branching.png", width = 10, height = 8)
